356bd290342cc271aff39c224409858238307734
emircankaragoz <emircan.krgz@gmail.com>
2023-04-25 10:00:46 +0300

    updated service

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,5 @@
 export { default as UserService } from './userService'
 export { default as TuzService } from './tuzService'
-<<<<<<< Updated upstream
 export { default as SuService } from './suService'
 export { default as AritmaService } from './aritmaService'
-=======
-export { default as IcmeSuyuService } from './icmeSuyuService'
-export { default as IsletmeSuyuService } from './isletmeSuyuService'
-export { default as YemekhaneSuyuService } from './yemekhaneSuyuService'
-export { default as RenkGidericiTuketimiService } from './renkGidericiTuketimiService'
-export { default as SaatlikVeriService } from './saatlikVeriService'
-export { default as CikisAtiksuSayacService } from './cikisAtiksuSayacService'
-export { default as TduService } from './tduService'
 export { default as SystemMessageService } from './systemMessageService'
->>>>>>> Stashed changes
